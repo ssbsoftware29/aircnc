183628e6918be65d23c9046f9e8b9b2344ef544b
Samuel Silva <samuelformal@gmail.com>
2020-01-16 16:08:51 -0300

    Criação dos outros modes e finalização da api rest

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,11 +4,18 @@ const upload = require('./config/upload');
 
 const sessionController = require('./controllers/SessionController');
 const spotController = require('./controllers/SpotController')
+const dashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 
 //rota do usuario, qual rota executar
 routes.post('/sessions', sessionController.store);
+routes.get('/spots', spotController.index);
 routes.post('/spots', upload.single('thumbnail') ,spotController.store);
+
+routes.get('/dashboard', dashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
